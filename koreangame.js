9597d5d2b7dde1e97ae0e4607a6b3e74bd27049f
sejinniemma <80943394+sejinniemma@users.noreply.github.com>
2021-06-18 22:12:32 +0900

    JS : button change & make timer logic is changed

--- koreangame.js
+++ koreangame.js
@@ -1,67 +1,81 @@
 'use srict'
 
-// 1.랜덤배열을 만들고 버튼을 누르면 1초마다 바뀌게 하기
-// 2.타이핑의 VALUE값이 배열의 인덱스번호와 같으면 점수증가
-// 3.버튼클릭후 타이머 변수설정후 줄이기 0일때 멈추기 
-// SCORe가 5이면 '합격'(TIEMR이내 OR 0) 타이머0,SORE<5 FAIL
-
-const starter = document.querySelector('.starter');
-const worldDisplay= document.querySelector('.world-display');
-const worldDisplayArray = ['바나나','사과','연필','밥','과일'];
+const starterBtn = document.querySelector('.starter');
+const wordDisplay = document.querySelector('.world-display');
+const wordDisplayArray = ['바나나','사과','연필','밥','과일'];
 const inputBox = document.querySelector('.word-input-box');
-const input = document.querySelector('input');
+const wordInput = document.querySelector('input');
 const time = document.querySelector('.time');
 const Score = document.querySelector('.score');
+
+let isPlaying = false;
+let timeInterval;
 let score = 0;
 let timeDuration=5; 
 
-starter.addEventListener('click', () => {
+
+starterBtn.addEventListener('click', () => {
+        buttonChange('Start!');
         displayItems();
         write();
-        Time();
+        run();
    })
+
+
 
    let i=0;
    function displayItems(){
    setInterval( ()=>{
-       if(i<worldDisplayArray.length){
-       worldDisplay.innerHTML = worldDisplayArray[i];
+       if(i<wordDisplayArray.length){
+       wordDisplay.innerHTML = wordDisplayArray[i];
        i++;
-       input.value="";
+       wordInput.value="";
+       
         } else {
-            worldDisplay.innerHTML = 'fail';
+            wordDisplay.innerHTML = 'fail';
         }
       } ,3000)
     }
 
-function write(){
-    input.addEventListener('input',(e)=>{
-     
-        if(worldDisplay.innerHTML===""){
+
+
+function write (){
+    wordInput.addEventListener('input',()=>{
+        if(wordDisplay.innerText===""){
           e.target.value="";
+         
         }
-        else if(e.target.value === worldDisplay.innerHTML){
-            Score.innerHTML = score + 1;
+        else if(wordInput.value === wordDisplay.innerText){
+            wordInput.value="";
             score++;
-            input.value="";
+            Score.innerHTML = score;
           }
-           else if(e.target.value !== worldDisplay.innerHTML){
-            return;
+          else if(wordInput.value !== wordDisplay.innerText){
+             return;
           }
+          
       })
     }
   
 
-
-function Time(){
-
-  let clock=setInterval(()=>{
-     if(timeDuration===0){
-      clearInterval(clock);
-      worldDisplay.innerHTML = 'fail';
-    }
-    time.innerHTML = timeDuration;
-    --timeDuration;
-  },3000)
+// Make timer
+function run(){
+  timeInterval = setInterval(countDown, 3000);
 }
 
+function countDown(){
+  timeDuration > 0 ? --timeDuration : isPlaying;
+     if(isPlaying){
+      clearInterval(timeInterval);
+      buttonChange('loading');
+     }
+    time.innerHTML = timeDuration;
+}
+
+
+
+// button change
+function buttonChange(text){
+  starterBtn.innerText = text;
+  text === 'Start!' ? starterBtn.classList.remove('loading') : starterBtn.classList.add('loading');
+}
